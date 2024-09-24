ff17274281c36d4880284555760e2a27067de958
MKngooo <s2410255.mizutani.cgp@gmail.com>
2024-09-24 22:56:07 +0900

    シーン削除
    シーン削除

--- Assets/Mizutani/Scripts/ClockOpen.cs
+++ Assets/Mizutani/Scripts/ClockOpen.cs
@@ -4,4 +4,14 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 
+public class ClockOpen : MonoBehaviour
+{
+    public GameObject ClockNazoPanel;
+    public GameObject ClockPanel;
+
+    public void OnClickClockOpenButton()
+    {
+        ClockNazoPanel.SetActive(true);
+        ClockPanel.SetActive(false);
+    }
 }
